2b757ea8d82ea9df50c334c86e8ff784ee29b3ce
Mateus Maso <m.maso25@gmail.com>
2014-07-31 18:26:17 -0400

    travis setup and fix grunt script for testing

--- spec/underscore.path.spec.js
+++ spec/underscore.path.spec.js
@@ -3,13 +3,11 @@ var _ = require("underscore");
 _.path = require('../src/underscore.path');
 
 describe("underscore.path", function() {
-  'use strict';
-
   beforeEach(function() {
     this.object = {foo: {bar: 123}};
   });
 
-  it("should work", function() {
+  it("should access nested property by keypath", function() {
     chai.expect(_.path(this.object, "foo.bar")).to.equal(123);
   });
 });
